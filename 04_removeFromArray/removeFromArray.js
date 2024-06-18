a4cf4d7787addaae28875727d9ba9803a48ad06a
RobertoMay <7630@itescam.edu.mx>
2024-06-18 12:47:12 -0600

    exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    return array.filter(element => !values.includes(element));
+
 };
 
 // Do not edit below this line
