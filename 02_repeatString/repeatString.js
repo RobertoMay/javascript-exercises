5939dc1c5cba63b08daa018a5d301a31cba0a6e6
RobertoMay <7630@itescam.edu.mx>
2024-06-18 12:21:48 -0600

    exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    let finalString = '';
+    for (let index = 0; index < number; index++) {
+        finalString = finalString +  string;
+    }
 
+    return finalString;
 };
 
 // Do not edit below this line
