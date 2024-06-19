3a6f852dc83a60fffae72366c58649d1ac9ed3e4
RobertoMay <7630@itescam.edu.mx>
2024-06-19 11:30:24 -0600

    exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(numFarenheit) {
+  return Number(((numFarenheit -= 32) * (5/9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(numCelcius) {
+  return Number(((numCelcius * (9/5)) + 32).toFixed(1));
 };
 
 // Do not edit below this line
