8bd453399e5638477aa4fd26bf8f4ae0f379d6be
RobertoMay <7630@itescam.edu.mx>
2024-06-25 12:20:02 -0600

    Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    const input = parseInt(n, 10);
+    let a = 0, b = 1, temp = 0;
+
+    if (input < 0) {
+        return "OOPS"
+    } else {
+        for (let i = 2; i <= input; i++) {
+            temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return input === 0 ? a : b;
+    }
 
 };
 
