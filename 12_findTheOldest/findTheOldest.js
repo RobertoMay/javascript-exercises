dbc995b33e527d21292ddefef3567fbca6841a56
RobertoMay <7630@itescam.edu.mx>
2024-06-25 12:38:05 -0600

    exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currentYear = new Date().getFullYear();
 
+    const greatestAge = array.reduce((mayor, persona) => {
+        const edadActual = persona.yearOfDeath ? persona.yearOfDeath - persona.yearOfBirth : currentYear - persona.yearOfBirth;
+        const mayorEdad = mayor.yearOfDeath ? mayor.yearOfDeath - mayor.yearOfBirth : currentYear - mayor.yearOfBirth;
+
+        return edadActual > mayorEdad ? persona : mayor;
+    });
+
+    return greatestAge;
 };
 
 // Do not edit below this line
