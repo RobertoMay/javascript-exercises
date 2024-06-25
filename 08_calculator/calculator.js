44561e77261d511a0ac21d9a57987c43bd062dc4
RobertoMay <7630@itescam.edu.mx>
2024-06-25 11:57:09 -0600

    exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function (number1, number2) {
+  return number1 + number2;
 };
 
-const subtract = function() {
-	
+const subtract = function (number1, number2) {
+  return number1 - number2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
+const multiply = function (array) {
+  return array.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
 
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  const aux = a;
+  for (let i = 1; i < b; i++) {
+    a *= aux;
+  }
+  return a;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a === 0 || a === 1) {
+    return 1;
+  } else {
+    const size = a-1;
+    let factorial = a;
+    for (let i = 0; i < size; i++) {
+      a--;
+      factorial = factorial * a;
+
+    }
+    return factorial;
+  }
+
 };
 
 // Do not edit below this line
