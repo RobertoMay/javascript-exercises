b44d9db21802ce17449141058dfe541c0c229ac4
RobertoMay <7630@itescam.edu.mx>
2024-06-25 12:02:08 -0600

    exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    const cleanedText = text.toLowerCase().replace(/[^a-z0-9]/g, '');
 
+    const reversedText = cleanedText.split('').reverse().join('');
+
+    return cleanedText === reversedText;
 };
 
 // Do not edit below this line
